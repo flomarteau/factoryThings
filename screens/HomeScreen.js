82c0b1d36f145fb29fcf0a0d36743d36ddcddf6b
flora <flora.marteau@live.fr>
2018-05-24 16:18:17 +0200

    enfin une nav qui marche

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
-  View
+  View,
+  Button
 } from 'react-native';
 import {Header} from 'react-native-elements';
 
@@ -20,6 +21,10 @@ export default class HomeScreen extends React.Component {
                 outerContainerStyles={{backgroundColor: 'black', marginTop: 20, height: 50}}
                 centerComponent={{ text: 'DASHBOARD', style: { color: '#ff8c00', fontSize: 20 } }}
               />
+        <Button
+          title="Go to map"
+          onPress={() => this.props.navigation.navigate('Map')}
+        />
       </View>
     );
   };
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,11 +2,16 @@ import React from 'react';
 import {
   ScrollView,
   Switch,
-  View
+  View,
+  Button,
+  TouchableOpacity
 } from 'react-native';
-import {Header} from 'react-native-elements'
+import {Header} from 'react-native-elements';
+
 import MapCards from '../components/MapCards';
 import MapGoogle from '../components/MapGoogle';
+
+import { Ionicons } from '@expo/vector-icons';
 
 export default class MapScreen extends React.Component {
   static navigationOptions = {
@@ -18,19 +23,25 @@ export default class MapScreen extends React.Component {
 
   render() {
     return (
-    <View>
-      <Header
-              innerContainerStyles={{backgroundColor: 'black'}}
-              outerContainerStyles={{backgroundColor: 'black', marginTop: 20, height: 50}}
-              centerComponent={{ text: 'MAP', style: { color: '#ff8c00', fontSize: 20} }}
-              rightComponent={<Switch onTintColor='#ff8c00' />}
-            />
+      <View>
+        <Header
+          innerContainerStyles={{backgroundColor: 'black'}}
+          outerContainerStyles={{backgroundColor: 'black', marginTop: 20, height: 50}}
+          centerComponent={{ text: 'MAP', style: { color: '#ff8c00', fontSize: 20} }}
+          rightComponent={<Switch onTintColor='#ff8c00' />}
+          leftComponent={
+            <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+              <Ionicons name="ios-arrow-back" size={22} color="#ff8c00" />
+            </TouchableOpacity>
+          }
+        />
 
-      <ScrollView>
-        <MapCards/>
-        <MapGoogle/>
-      </ScrollView>
-    </View>
+        <ScrollView>
+          <MapCards/>
+          <MapGoogle/>
+        </ScrollView>
+
+      </View>
 
     );
   }
